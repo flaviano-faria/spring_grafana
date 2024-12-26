c146ae6978c7a18319870414c3c474d64fe24c64
flaviano-faria <flsilv89@yahoo.com.br>
2024-12-25 23:32:16 -0300

    initial commit

---
+++ src/main/java/com/grafana/app/SpringGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.grafana.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grafana/app/SpringGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grafana.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
