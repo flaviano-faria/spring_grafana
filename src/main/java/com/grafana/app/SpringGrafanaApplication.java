e94071b465216bb3e2de822bccfd183aa6ce50cd
flaviano-faria <flsilv89@yahoo.com.br>
2024-12-27 07:08:20 -0300

    Creating logService

--- src/main/java/com/grafana/app/SpringGrafanaApplication.java
+++ src/main/java/com/grafana/app/SpringGrafanaApplication.java
@@ -3,11 +3,15 @@ package com.grafana.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.grafana.service.LogService;
+
 @SpringBootApplication
 public class SpringGrafanaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGrafanaApplication.class, args);
+		LogService service = new LogService();
+		service.log();
 	}
 
 }
---
+++ src/main/java/com/grafana/service/LogService.java
@@ -0,0 +1,15 @@
+package com.grafana.service;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogService {
+
+	 private final Logger LOG = (Logger) LoggerFactory.getLogger(LogService.class);
+
+	    public void log(){
+	        LOG.info("LogService is logging");
+	    }
+}
