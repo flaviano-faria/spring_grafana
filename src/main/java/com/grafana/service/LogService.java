d2f69f563cb229194c48a4ec545d13666914ecd8
flaviano-faria <flsilv89@yahoo.com.br>
2024-12-29 15:09:24 -0300

    updating log message

--- src/main/java/com/grafana/service/LogService.java
+++ src/main/java/com/grafana/service/LogService.java
@@ -1,5 +1,7 @@
 package com.grafana.service;
 
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,14 @@ public class LogService {
 	 private final Logger LOG = (Logger) LoggerFactory.getLogger(LogService.class);
 
 	    public void log(){
-	        LOG.info("LogService is logging");
+	        LOG.info("{\r\n"
+	        		+ "	\"id\":13456,\r\n"
+	        		+ "	\"code\":\""+generateUUID()+"\",\r\n"
+	        		+ "	\"message\":\"success\"\r\n"
+	        		+ "}");
+	    }
+	    
+	    private String generateUUID(){
+	    	return UUID.randomUUID().toString().replace("-", "");
 	    }
 }
