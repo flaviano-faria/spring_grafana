fd43693e8fd1f196d34afbe722a7c42aaa162da4
flaviano-faria <flsilv89@yahoo.com.br>
2025-01-13 21:17:52 -0300

    generate logSequence method]

--- src/main/java/com/grafana/app/SpringGrafanaApplication.java
+++ src/main/java/com/grafana/app/SpringGrafanaApplication.java
@@ -11,7 +11,7 @@ public class SpringGrafanaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGrafanaApplication.class, args);
 		LogService service = new LogService();
-		service.log();
+		service.generateLogSequence();
 	}
 
 }
--- src/main/java/com/grafana/service/LogService.java
+++ src/main/java/com/grafana/service/LogService.java
@@ -1,5 +1,7 @@
 package com.grafana.service;
 
+import java.util.Iterator;
+import java.util.Random;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -17,9 +19,26 @@ public class LogService {
 	        		+ "	\"code\":\""+generateUUID()+"\",\r\n"
 	        		+ "	\"message\":\"success\"\r\n"
 	        		+ "}");
+	        
+	        
+	        LOG.info("{\r\n"
+	        		+ "	\"total\":"+generateRandomInt()+"\"\r\n"
+	        		+ "}");
+	    }
+	    
+	    public void generateLogSequence() {
+	    	for (int i=0; i<4; i++) {
+				log();			
+				
+			}
 	    }
 	    
 	    private String generateUUID(){
 	    	return UUID.randomUUID().toString().replace("-", "");
+	    }
+	    
+	    private double generateRandomInt() {
+	    	Random random = new Random();
+	    	return random.nextDouble();
 	    }
 }
